ce361ef0184857fca0b2fbad85c602e550996a82
Chornohub <chornohub@github.com>
2024-06-18 12:09:00 +0400

    Change port due to possible Mac conflicts

--- MovieRecommender/MovieRecommenderApp.swift
+++ MovieRecommender/MovieRecommenderApp.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 @main
 struct MovieRecommenderApp: App {
-    let viewModel = MainViewModel(networkManager: .live("http://127.0.0.1:5000"))
+    let viewModel = MainViewModel(networkManager: .live("http://127.0.0.1:8080"))
 //    let viewModel = MainViewModel(networkManager: .preview)
     
     var body: some Scene {
