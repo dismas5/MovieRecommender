4d06d5c900239db68eb73b4a3487c63a1c00d8b8
Chornohub <chornohub@github.com>
2024-06-18 11:53:51 +0400

    Implement locally working model

--- MovieRecommender/main.py
+++ MovieRecommender/main.py
@@ -1,97 +1,95 @@
-import random
-import numpy as np
 import pandas as pd
-from flask import Flask, request, jsonify
-from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+import tensorflow as tf
 
-app = Flask(__name__)
-
-ratings = pd.read_csv('ml-100k/u.data', sep='\t', names=['user_id', 'movie_id', 'rating', 'timestamp'])
-movies = pd.read_csv('ml-100k/u.item', sep='|', encoding='latin-1', header=None,
-                     names=['movie_id', 'title', 'release_date', 'video_release_date', 'IMDb_URL', 
-                            'unknown', 'Action', 'Adventure', 'Animation', 'Children\'s', 'Comedy', 
-                            'Crime', 'Documentary', 'Drama', 'Fantasy', 'Film-Noir', 'Horror', 
-                            'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western'])
-
-# Combine genres for each movie
-movies['genres'] = movies[['Action', 'Adventure', 'Animation', 'Children\'s', 'Comedy', 'Crime',
-                           'Documentary', 'Drama', 'Fantasy', 'Film-Noir', 'Horror', 'Musical',
-                           'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western']].apply(
-                           lambda row: ', '.join(row.index[row == 1]), axis=1)
-
-movies = movies[['movie_id', 'title', 'genres']]
-data = pd.merge(ratings, movies, on='movie_id')
-
-# Encode genres for embeddings
-genre_encoding = pd.get_dummies(movies['genres'])
-movies = movies.join(genre_encoding)
-movie_embeddings = movies.set_index('movie_id')[genre_encoding.columns].values
-
-user_ratings = {}
-
-def recommend_movie(user_ratings, movie_embeddings, epsilon=0.1, exploration_rate=0.2):
-    rated_movies = list(user_ratings.keys())
-    unrated_movies = [movie for movie in range(len(movie_embeddings)) if movie + 1 not in rated_movies]
-
-    if not rated_movies:
-        return random.choice(unrated_movies) + 1
-
-    rated_genres = set(movies.loc[movies['movie_id'].isin(rated_movies), 'genres'])
-    all_genres = set(movies['genres'])
-    unrated_genres = all_genres - rated_genres
-
-    rated_embeddings = np.array([movie_embeddings[movie - 1] for movie in rated_movies])
-    mean_embedding = np.mean(rated_embeddings, axis=0).reshape(1, -1)
-    similarities = cosine_similarity(mean_embedding, movie_embeddings[unrated_movies])
-    similarity_scores = similarities.flatten()
-
-    for movie_id, rating in user_ratings.items():
-        genre = movies.loc[movies['movie_id'] == movie_id, 'genres'].values[0]
-        genre_col = genre_encoding.columns.get_loc(genre)
-        if rating <= 2:
-            genre_adjustment = movie_embeddings[unrated_movies, genre_col]
-            similarity_scores -= genre_adjustment * (3 - rating)
-
-    exploration_indices = [i for i, movie_id in enumerate(unrated_movies)
-                           if any(genre in movies.loc[movies['movie_id'] == movie_id + 1, 'genres'].values[0].split(', ')
-                                  for genre in unrated_genres)]
-
-    if random.random() < epsilon:
-        if exploration_indices and random.random() < exploration_rate:
-            next_movie = unrated_movies[random.choice(exploration_indices)]
-        else:
-            next_movie = random.choice(unrated_movies)
-    else:
-        if exploration_indices and random.random() < exploration_rate:
-            next_movie = unrated_movies[random.choice(exploration_indices)]
-        else:
-            next_movie = unrated_movies[np.argmax(similarity_scores)]
+class RecommenderNet(tf.keras.Model):
+    def __init__(self, num_users, num_movies, embedding_size, **kwargs):
+        super(RecommenderNet, self).__init__(**kwargs)
+        self.num_users = num_users
+        self.num_movies = num_movies
+        self.user_embedding = tf.keras.layers.Embedding(num_users + 1, embedding_size, embeddings_initializer='he_normal')
+        self.user_bias = tf.keras.layers.Embedding(num_users + 1, 1)
+        self.movie_embedding = tf.keras.layers.Embedding(num_movies + 1, embedding_size, embeddings_initializer='he_normal')
+        self.movie_bias = tf.keras.layers.Embedding(num_movies + 1, 1)
     
-    return next_movie + 1
+    def call(self, inputs):
+        user_vector = self.user_embedding(inputs[:, 0])
+        user_bias = self.user_bias(inputs[:, 0])
+        movie_vector = self.movie_embedding(inputs[:, 1])
+        movie_bias = self.movie_bias(inputs[:, 1])
+        
+        dot_user_movie = tf.tensordot(user_vector, movie_vector, 2)
+        
+        x = dot_user_movie + user_bias + movie_bias
+        
+        return tf.nn.sigmoid(x)
 
-@app.route('/next_movie', methods=['GET'])
-def get_next_movie():
-    movie_id = recommend_movie(user_ratings, movie_embeddings, epsilon=0.05, exploration_rate=0.2)
-    movie_details = movies[movies['movie_id'] == movie_id][['movie_id', 'title', 'genres']].to_dict(orient='records')[0]
-    movie_details['genres'] = movie_details['genres'].split(', ')
-    return jsonify(movie_details)
+# Load the model
+model = tf.keras.models.load_model('recommendation_model', custom_objects={'RecommenderNet': RecommenderNet})
 
-@app.route('/get_movies_by_genre', methods=['GET'])
-def get_movies_by_genre(genre: str = "Animation"):
-    movie_details = movies[movies['genres'].str.contains(genre)][['movie_id', 'title', 'genres']].to_dict(orient='records')
-    return jsonify(movie_details)
+# Reload the original dataset and movie titles
+column_names = ['user_id', 'movie_id', 'rating', 'timestamp']
+data = pd.read_csv('ml-100k/u.data', sep='\t', names=column_names)
+movie_titles = pd.read_csv('ml-100k/u.item', sep='|', encoding='latin-1', usecols=[0, 1], names=['movie_id', 'title'])
 
-@app.route('/all_movies', methods=['GET'])
-def get_all_movies():
-    return jsonify(movies[['movie_id', 'title', 'genres']].to_dict(orient='records'))
+data = pd.merge(data, movie_titles, on='movie_id')
+data['user_id'] = data['user_id'].astype('category').cat.codes.values
+data['movie_id'] = data['movie_id'].astype('category').cat.codes.values
+data['rating'] = data['rating'] / 5.0  # Normalize ratings
 
-@app.route('/rate_movie', methods=['POST'])
-def rate_movie():
-    data = request.json
-    movie_id = data['movie_id']
-    rating = data['rating']
-    user_ratings[movie_id] = rating
-    return jsonify({'message': 'Rating received'}), 200
+# Function to add new user preferences and retrain the model temporarily
+def add_new_user_preferences(data, new_user_preferences, movie_titles):
+    new_user_ratings = pd.DataFrame(new_user_preferences)
+    movie_to_id = {v: k for k, v in movie_titles[['movie_id', 'title']].to_dict('split')['data']}
+    new_user_ratings['movie_id'] = new_user_ratings['movie_title'].map(movie_to_id)
+    new_user_ratings['user_id'] = data['user_id'].max() + 1
+    new_user_ratings['rating'] = new_user_ratings['user_rating'] / 5.0
+    new_user_ratings = new_user_ratings[['user_id', 'movie_id', 'rating']]
+    augmented_data = pd.concat([data, new_user_ratings], ignore_index=True)
+    return augmented_data, new_user_ratings['user_id'].iloc[0]
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+# Function to recommend movies
+def recommend_movies(model, data, new_user_preferences, movie_titles, top_k=10, retrain_epochs=5):
+    augmented_data, new_user_id = add_new_user_preferences(data, new_user_preferences, movie_titles)
+    train_data = tf.data.Dataset.from_tensor_slices((augmented_data[['user_id', 'movie_id']].values.astype(np.int16), augmented_data['rating'].values))
+    batch_size = 64
+    train_data = train_data.shuffle(len(augmented_data)).batch(batch_size)
+    
+    # Define a new optimizer with a possibly different learning rate
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+        loss=tf.keras.losses.BinaryCrossentropy()
+    )
+
+    model.fit(train_data, epochs=retrain_epochs, verbose=1)
+    
+    all_movies = movie_titles['movie_id'].values
+    user_movie_array = np.hstack((np.array([new_user_id] * len(all_movies)).reshape(-1, 1), all_movies.reshape(-1, 1))).astype(np.int16)
+    
+    predictions = model.predict(user_movie_array).flatten()
+    top_indices = predictions.argsort()[-top_k:][::-1]
+    recommended_movie_ids = all_movies[top_indices]
+    recommended_movies = movie_titles[movie_titles['movie_id'].isin(recommended_movie_ids)]
+    return recommended_movies['title'].values
+
+# Example usage
+# new_user_preferences = [
+#     {"movie_title": "Lion King, The (1994)", "user_rating": 5.0},
+#     {"movie_title": "Akira (1988)", "user_rating": 5.0},
+#     {"movie_title": "Cinderella (1950)", "user_rating": 4.0},
+#     {"movie_title": "Aladdin and the King of Thieves (1996)", "user_rating": 4.0},
+#     {"movie_title": "Dumbo (1941)", "user_rating": 4.0}
+# ]
+
+new_user_preferences = [
+    {"movie_title": "Star Wars (1977)", "user_rating": 5.0},
+    {"movie_title": "Stargate (1994)", "user_rating": 5.0},
+    {"movie_title": "Robert A. Heinlein's The Puppet Masters (1994)", "user_rating": 4.0},
+    {"movie_title": "Jurassic Park (1993)", "user_rating": 4.0},
+    {"movie_title": "Twelve Monkeys (1995)", "user_rating": 4.0},
+    {"movie_title": "Terminator 2: Judgment Day (1991)", "user_rating": 4.0}
+]
+
+recommendations = recommend_movies(model, data, new_user_preferences, movie_titles)
+print(recommendations)
+
